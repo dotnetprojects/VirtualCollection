5fa7da9ab6ea32702c42b4b074f133f36a046ace
jogibear9988 <jochen.kuehner@gmx.de>
2017-06-14 10:11:05 +0200

    wip EditableCollectionView

--- VirtualCollection/VirtualCollection_IEditableCollectionView.cs
+++ VirtualCollection/VirtualCollection_IEditableCollectionView.cs
@@ -10,55 +10,73 @@ namespace VirtualCollection
     {
         public object AddNew()
         {
-            throw new NotImplementedException();
+            CurrentAddItem = ItemAdd?.Invoke();
+            IsAddingNew = true;
+            return CurrentAddItem;
         }
 
         public void CommitNew()
         {
+            ItemAddFinished?.Invoke(CurrentAddItem);
+            IsAddingNew = false;
+            CurrentAddItem = null;
         }
 
         public void CancelNew()
         {
-            throw new NotImplementedException();
+            ItemAddCanceled?.Invoke(CurrentAddItem);
+            IsAddingNew = false;
+            CurrentAddItem = null;
         }
 
         void IEditableCollectionView.Remove(object item)
         {
-            throw new NotImplementedException();
+            ItemRemoved?.Invoke(CurrentEditItem);
         }
 
         public void EditItem(object item)
         {
             CurrentEditItem = item;
+            ItemEditStarted?.Invoke(item);
             IsEditingItem = true;
         }
 
         public void CommitEdit()
         {
+            ItemEditFinished?.Invoke(CurrentEditItem);
             CurrentEditItem = null;
             IsEditingItem = false;
         }
 
         public void CancelEdit()
         {
+            ItemEditCanceled?.Invoke(CurrentEditItem);
             CurrentEditItem = null;
             IsEditingItem = false;
         }
 
         public NewItemPlaceholderPosition NewItemPlaceholderPosition { get; set; }
 
-        public bool CanAddNew { get; private set; }
+        public bool CanAddNew { get; set; }
 
         public bool IsAddingNew { get; private set; }
 
         public object CurrentAddItem { get; private set; }
 
-        public bool CanRemove { get; private set; }
+        public bool CanRemove { get; set; }
 
-        public bool CanCancelEdit { get; private set; }
+        public bool CanCancelEdit { get; set; }
 
         public bool IsEditingItem { get; private set; }
 
         public object CurrentEditItem { get; private set; }
+
+        public event Action<object> ItemEditStarted;
+        public event Action<object> ItemEditFinished;
+        public event Action<object> ItemEditCanceled;
+        public event Action<object> ItemRemoved;
+        public event Func<object> ItemAdd;
+        public event Action<object> ItemAddFinished;
+        public event Action<object> ItemAddCanceled;
     }
 }
